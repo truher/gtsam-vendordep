553623bebc9f3f29dd7c3733c73ea784342d5257
Joel Truher <joel@truher.org>
2026-06-06 15:52:10 -0700

    WIP

--- src/main/java/gtsam/noiseModel/mEstimator/Base.java
+++ src/main/java/gtsam/noiseModel/mEstimator/Base.java
@@ -12,7 +12,8 @@ import org.team100.foreign.Lib;
 
 public class Base extends ForeignObject {
     public enum FF {
-        noiseModel_mEstimator_Base_weight(JAVA_DOUBLE, ADDRESS, JAVA_DOUBLE);
+        noiseModel_mEstimator_Base_weight(JAVA_DOUBLE, ADDRESS, JAVA_DOUBLE),
+        noiseModel_mEstimator_Base_loss(JAVA_DOUBLE, ADDRESS, JAVA_DOUBLE);
 
         public final MethodHandle h;
 
@@ -27,6 +28,10 @@ public class Base extends ForeignObject {
 
     public double weight(double distance) throws Throwable {
         return (double) FF.noiseModel_mEstimator_Base_weight.h.invokeExact(ptr, distance);
+    }
+
+    public double loss(double distance) throws Throwable {
+        return (double) FF.noiseModel_mEstimator_Base_loss.h.invokeExact(ptr, distance);
     }
 
 }
